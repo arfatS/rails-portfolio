6e49db1a6e1a4d14eab81cd2d876cec10cff8aff
arfatS <arfatsarole@gmail.com>
2021-04-25 13:07:23 +0530

    created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+    Blog.create!(
+        "title" => "My blog post #{blog}",
+        "body" => "Do you have an eye for news and an interest in sharing that news in a unique and fascinating way? Although you certainly don’t want to just regurgitate the same material out there on every news site, you may want to share the latest information about a specific topic or even report the news through a comedic filter."
+    )
+end
+puts "10 blog posts created via seeds."
+
+5.times do |skill|
+    Skill.create!(
+        "title" => "Rails #{skill}",
+        "percent_utilized" => skill * 7
+    )
+end
+puts "5 skills created via seeds."
+
+9.times do |item|
+    Portfolio.create!(
+        "title" => "Portfolio title #{item}",
+        "subtitle" => "My portfolio service",
+        "body" => "Do you have an eye for news and an interest in sharing that news in a unique and fascinating way? Although you certainly don’t want to just regurgitate the same material out there on every news site, you may want to share the latest information about a specific topic or even report the news through a comedic filter.",
+        "main_image" => "https://placeimg.com/600/400/tech",
+        "thumb_image" => "https://placeimg.com/350/200/tech"
+    )
+end
+puts "9 portfolio items created via seeds."
