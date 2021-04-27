ca40bc6ef4455b8555155861cc9ef094df95c374
arfatS <arfatsarole@gmail.com>
2021-04-27 16:35:22 +0530

    show and delete actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,4 +38,18 @@ class PortfoliosController < ApplicationController
           end
         end
     end
+
+    def show
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def destroy
+        @portfolio_item = Portfolio.find(params[:id])
+
+        @portfolio_item.destroy
+        respond_to do |format|
+            format.html { redirect_to portfolios_url, notice: "Portfolio was successfully destroyed." }
+            format.json { head :no_content }
+        end
+    end
 end
